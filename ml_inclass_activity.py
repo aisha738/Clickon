6733035347f4334a4e68e6eb3e5ae043a3dafa92
Aisha Abdulkarim <105824064+aisha738@users.noreply.github.com>
2025-02-27 12:13:51 +0100

    Update ml_inclass_activity.py

--- ml_inclass_activity.py
+++ ml_inclass_activity.py
@@ -13,8 +13,7 @@ import streamlit as st
 
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
+import pickle
 
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler, LabelEncoder
