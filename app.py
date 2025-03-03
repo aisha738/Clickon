1e2f858db3e1d651b8eb5e5640052df9c92760eb
Aisha Abdulkarim <105824064+aisha738@users.noreply.github.com>
2025-03-03 23:20:47 +0100

    Update app.py

--- app.py
+++ app.py
@@ -67,8 +67,8 @@
 
 import pandas as pd
 import numpy as np
-#import matplotlib.pyplot as plt
-#import seaborn as sns
+import matplotlib.pyplot as plt
+import seaborn as sns
 import streamlit as st
 
 from sklearn.model_selection import train_test_split
